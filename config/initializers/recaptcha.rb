acf96a045203931e7dbc6dd3c74f32093a4024f9
Milly <milly.chuang0624@gmail.com>
2016-08-01 05:45:08 +0800

    Add recaptach config

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6Ld4tCgTAAAAAAMROf12Nmb1BerbPOPCkUZf23Hw'
+  config.private_key = '6Ld4tCgTAAAAANuObHTXhgtP6EnT9ODUpWSzf_rc'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
