09defe289314aec31a0c045033b1cc713d16d5a2
Milly <milly.chuang0624@gmail.com>
2018-02-20 14:44:09 +0800

    Change server name, and remove post header

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Ld4tCgTAAAAAAMROf12Nmb1BerbPOPCkUZf23Hw'
-  config.private_key = '6Ld4tCgTAAAAANuObHTXhgtP6EnT9ODUpWSzf_rc'
+  config.site_key  = '6Ld4tCgTAAAAAAMROf12Nmb1BerbPOPCkUZf23Hw'
+  config.secret_key = '6Ld4tCgTAAAAANuObHTXhgtP6EnT9ODUpWSzf_rc'
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
